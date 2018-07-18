c932cda66f543f7aff395230dcbe44e1c53e59b7
Markus Friedrich <friedrich.at.gc@googlemail.com>
2018-07-18 21:26:13 +0200

    Fixed cookiewarning message

--- buildSystem/html/cookiewarning.js
+++ buildSystem/html/cookiewarning.js
@@ -2,11 +2,11 @@ if($(location).attr('href').startsWith("https://www.mbsim-env.de")>=0) {
   $(document).ready(function() {
     if(!localStorage.getItem('cookieWarning')) {
       $("body").prepend(
-  `<nav class="navbar navbar-inverse navbar-fixed-bottom" id="COOKIEWARNING">
+  `<nav class="navbar navbar-inverse navbar-fixed-top" id="COOKIEWARNING">
     <div class="container-fluid">
       <p class="navbar-text">This website uses Cookies. We assume you are fine with this.
-      <a href="/mbsim/html/impressum_disclaimer_datenschutz.html#datenschutz">Read more.</a></p>
-      <button type="button" id="COOKIEBUTTON" class="btn btn-default navbar-btn">OK</button>
+      <a href="/mbsim/html/impressum_disclaimer_datenschutz.html#datenschutz">Read more.</a>
+      <button type="button" id="COOKIEBUTTON" class="btn btn-default navbar-btn">OK</button></p>
     </div>
   </nav>`);
     }
