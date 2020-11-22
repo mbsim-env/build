b781addaf80e0fd7331b9b47b71852bdfa8eb1ba
Markus Friedrich <friedrich.at.gc@googlemail.com>
2020-11-22 16:40:04 +0100

    Fix

--- docker/webserverImage/cron-ci.py
+++ docker/webserverImage/cron-ci.py
@@ -41,7 +41,7 @@ waitTime=datetime.timedelta(minutes=1) if os.environ["MBSIMENVTAGNAME"]!="stagin
 
 ciq=service.models.CIQueue.objects.all().order_by("recTime").first()
 if ciq is not None:
-  if ciq.fmatvecBranch and ciq.hdf5serieBranch and ciq.openmbvBranch and ciq.mbsimBranch:
+  if ciq.fmatvecBranch is not None and ciq.hdf5serieBranch is not None and ciq.openmbvBranch is not None and ciq.mbsimBranch is not None:
     print("Found something to build: "+ciq.fmatvecBranch+", "+ciq.hdf5serieBranch+", "+\
           ciq.openmbvBranch+", "+ciq.mbsimBranch+" starting in, at most, "+str(waitTime))
     sys.stdout.flush()
@@ -50,15 +50,15 @@ if ciq is not None:
             ciq.openmbvBranch+", "+ciq.mbsimBranch)
       sys.stdout.flush()
       # run linux64-ci
+      ciq.delete()
       ret=setup.run("build-linux64-ci", args.jobs, printLog=False,
                     fmatvecBranch=ciq.fmatvecBranch, hdf5serieBranch=ciq.hdf5serieBranch,
                     openmbvBranch=ciq.openmbvBranch, mbsimBranch=ciq.mbsimBranch)
-      ciq.delete()
       sys.exit(ret)
-  if ciq.buildCommitID:
+  if ciq.buildCommitID is not None:
     print("Start build of build-system: "+ciq.buildCommitID)
     sys.stdout.flush()
     # run rebuild build-system
-    ret=setup.run("builddocker", args.jobs, printLog=False, builddockerBranch=ciq.buildCommitID)
     ciq.delete()
+    ret=setup.run("builddocker", args.jobs, printLog=False, builddockerBranch=ciq.buildCommitID)
     sys.exit(ret)
