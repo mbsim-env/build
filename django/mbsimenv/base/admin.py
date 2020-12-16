63f34e73eec0074238984f7d9482cfd18586cc35
Markus Friedrich <friedrich.at.gc@googlemail.com>
2020-12-16 20:37:52 +0100

    Avoid combobox for Foreign keys in admin page.
    Showing a combobox is very expensive is it has many many entries (=
    possible Foreign key ids)

--- django/mbsimenv/base/admin.py
+++ django/mbsimenv/base/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+import django
 
-# Register your models here.
+# admin model
+class MBSimEnvModelAdmin(django.contrib.admin.ModelAdmin):
+  raw_id_fields = ()
+  def __init__(self, model, admin_site, *args, **kwargs):
+    self.raw_id_fields = tuple( f.name for f in model._meta.get_fields() if isinstance(f, django.db.models.ForeignKey) )
+    super().__init__(model, admin_site, *args, **kwargs)
--- django/mbsimenv/builds/admin.py
+++ django/mbsimenv/builds/admin.py
@@ -1,5 +1,6 @@
 import django
 import builds
+import base
 
-django.contrib.admin.site.register(builds.models.Run)
-django.contrib.admin.site.register(builds.models.Tool)
+django.contrib.admin.site.register(builds.models.Run, base.admin.MBSimEnvModelAdmin)
+django.contrib.admin.site.register(builds.models.Tool, base.admin.MBSimEnvModelAdmin)
