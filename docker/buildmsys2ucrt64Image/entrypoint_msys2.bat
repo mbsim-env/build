034730da5198925cfe88fe9515ee16f9a0339399
Markus Friedrich <fm12111+github@googlemail.com>
2024-05-20 16:49:43 +0200

    msys tet

--- docker/buildmsys2ucrt64Image/entrypoint_msys2.bat
+++ docker/buildmsys2ucrt64Image/entrypoint_msys2.bat
@@ -1,9 +1,14 @@
 @echo off
 echo mfmfa
-dir c:\msys64\usr\bin\bash.exe
-echo "%*"
-c:\msys64\usr\bin\bash.exe -l -c "ls -l /ucrt64/bin/python3*"
-c:\msys64\usr\bin\bash.exe -l -c "ls -l /context/entrypoint.py"
-echo mfmfa
-c:\msys64\usr\bin\bash.exe -l -c "%*"
+cd
 echo mfmfb
+dir c:\msys64\usr\bin\bash.exe
+echo mfmfc
+echo "%*"
+echo mfmfd
+c:\msys64\usr\bin\bash.exe -l -c "ls -l /ucrt64/bin/python3*"
+echo mfmfe
+c:\msys64\usr\bin\bash.exe -l -c "ls -l /context/entrypoint.py"
+echo mfmff
+c:\msys64\usr\bin\bash.exe -l -c "%*"
+echo mfmfg
