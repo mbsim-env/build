d0a7533956f2734fd9ad69ef743bf01f3af6db63
Markus Friedrich <fm12111+github@googlemail.com>
2024-05-20 08:21:46 +0200

    msys2 test

--- docker/buildmsys2ucrt64Image/entrypoint.py
+++ docker/buildmsys2ucrt64Image/entrypoint.py
@@ -7,6 +7,8 @@ import subprocess
 import sys
 import psutil
 import django
+import time
+import json
 sys.path.append("c:/msys64/context/mbsimenv")
 import service
 
@@ -26,6 +28,7 @@ argparser.add_argument("--jobs", "-j", type=int, default=psutil.cpu_count(False)
 argparser.add_argument('--forceBuild', action="store_true", help="Passed to buily.py if existing")
 argparser.add_argument("--ccacheSize", default=10, type=int, help="Maximal ccache size in GB.")
 argparser.add_argument("--buildRunID", default=None, type=int, help="The id of the builds.model.Run dataset this example run belongs to.")
+argparser.add_argument("--buildConfig", type=json.loads, default={}, help="Load an additional build(/examples) configuration as json string")
 
 args=argparser.parse_args()
 
@@ -34,11 +37,10 @@ if "MBSIMENVSERVERNAME" not in os.environ or os.environ["MBSIMENVSERVERNAME"]=="
   raise RuntimeError("Envvar MBSIMENVSERVERNAME is not defined.")
 
 # check buildtype
-if args.buildType != "win64-ci" and not args.buildType.startswith("win64-dailyrelease"):
+if not args.buildType.startswith("msys2win64"):
   raise RuntimeError("Unknown build type "+args.buildType+".")
 
-#mfmfos.environ["DJANGO_SETTINGS_MODULE"]="mbsimenv.settings_buildsystem"
-os.environ["DJANGO_SETTINGS_MODULE"]="mbsimenv.settings_local"
+os.environ["DJANGO_SETTINGS_MODULE"]="mbsimenv.settings_buildsystem"
 django.setup()
 
 # wait for database server
@@ -55,18 +57,16 @@ django.db.connections.close_all()
 # run
 
 # clone repos if needed
-if not os.path.isdir("/mbsim-env/fmatvec"):
-  subprocess.check_call(["git", "clone", "-q", "--depth", "1", "https://github.com/mbsim-env/fmatvec.git"], cwd="/mbsim-env",
-    stdout=sys.stdout, stderr=sys.stderr)
-if not os.path.isdir("/mbsim-env/hdf5serie"):
-  subprocess.check_call(["git", "clone", "-q", "--depth", "1", "https://github.com/mbsim-env/hdf5serie.git"], cwd="/mbsim-env",
-    stdout=sys.stdout, stderr=sys.stderr)
-if not os.path.isdir("/mbsim-env/openmbv"):
-  subprocess.check_call(["git", "clone", "-q", "--depth", "1", "https://github.com/mbsim-env/openmbv.git"], cwd="/mbsim-env",
-    stdout=sys.stdout, stderr=sys.stderr)
-if not os.path.isdir("/mbsim-env/mbsim"):
-  subprocess.check_call(["git", "clone", "-q", "--depth", "1", "https://github.com/mbsim-env/mbsim.git"], cwd="/mbsim-env",
-    stdout=sys.stdout, stderr=sys.stderr)
+for repo in [
+              "https://github.com/mbsim-env/fmatvec.git",
+              "https://github.com/mbsim-env/hdf5serie.git",
+              "https://github.com/mbsim-env/openmbv.git",
+              "https://github.com/mbsim-env/mbsim.git",
+            ]+list(map(lambda repo: repo["gitURL"], args.buildConfig.get("addRepos", []))):
+  localDir=repo.split("/")[-1][0:-4]
+  if not os.path.isdir("c:/msys64/mbsim-env/"+localDir):
+      subprocess.check_call(["git", "clone", "-q", "--depth", "1", repo], cwd="c:/msys64/mbsim-env",
+      stdout=sys.stdout, stderr=sys.stderr)
 
 # args
 if args.buildType.startswith("win64-dailyrelease"):
@@ -119,12 +119,13 @@ if args.buildRunID is not None:
 # run build
 ret=subprocess.call(
   ["c:/msys64/ucrt64/bin/python.exe", "c:/mbsim-env/build/django/mbsimenv/build.py"]+ARGS+[
-  "--sourceDir", "c:/mbsim-env", "--binSuffix=-dockermsys2ucrt64", "--prefix", "c:/mbsim-env/local-dockermsys2ucrt64", "-j", str(args.jobs), "--buildSystemRun",
+  "--sourceDir", "c:/msys64/mbsim-env", "--binSuffix=-dockermsys2ucrt64", "--prefix", "c:/msys64/mbsim-env/local-dockermsys2ucrt64", "-j", str(args.jobs), "--buildSystemRun",
   "--fmatvecBranch", args.fmatvecBranch,
   "--hdf5serieBranch", args.hdf5serieBranch, "--openmbvBranch", args.openmbvBranch,
   "--mbsimBranch", args.mbsimBranch, "--enableCleanPrefix",
   "--buildType", args.buildType,
   "--executor", args.executor,
+  "--buildConfig", json.dumps(args.buildConfig),
   "--passToConfigure", "--enable-shared", "--disable-static", "--enable-python",
   "--with-boost-system-lib=boost_system-mt",
   "--with-boost-filesystem-lib=boost_filesystem-mt",
