91b1f2601f4def65836a069b77f107b9d0fd135f
Markus Friedrich <fm12111+github@googlemail.com>
2025-06-06 12:49:29 +0200

    Added missing file

---
+++ django/mbsimenv/runexamples-fmu.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import argparse
+import fmpy
+import os
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument("unzipdir", type=os.path.abspath)
+meOrCosim = argparser.add_mutually_exclusive_group()
+meOrCosim.add_argument('--me', action='store_true')
+meOrCosim.add_argument('--cosim', action='store_false')
+args = argparser.parse_args()
+
+md = fmpy.read_model_description("mbsim.fmu")
+if args.me:
+  fmu = fmpy.fmi1.FMU1Model(guid=md.guid, modelIdentifier=md.modelExchange.modelIdentifier, unzipDirectory=args.unzipdir)
+else:
+  fmu = fmpy.fmi1.FMU1Slave(guid=md.guid, modelIdentifier=md.coSimulation.modelIdentifier, unzipDirectory=args.unzipdir)
+fmu.instantiate()
+fmu.initialize()
+fmu.terminate()
+fmu.freeInstance()
